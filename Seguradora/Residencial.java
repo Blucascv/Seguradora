7bc3172df56b5a90b3aff7b49cc65034276ba960
Blucascv <blucascv@gmail.com>
2017-05-16 08:29:26 -0300

    Update Residencial.java

--- Seguradora/Residencial.java
+++ Seguradora/Residencial.java
@@ -3,8 +3,11 @@ package Seguradora;
 public class Residencial extends Contrato{
 	
 	private String endImovel;
-	private String zonaImovel;
-	private String tipoResidencia;
+	private int zonaImovel;//1-Urbana; 2-Sub-Urbana; 3-Rural
+	private int tipoResidencia;//1-Apartamento; 2-Casa
+	
+	/*private String zonaImovel;//1-Urbana; 2-Sub-Urbana; 3-Rural
+	private String tipoResidencia;//1-Apartamento; 2-Casa*/
 	
 	public String getEndImovel() {
 		return endImovel;
@@ -12,16 +15,16 @@ public class Residencial extends Contrato{
 	public void setEndImovel(String endImovel) {
 		this.endImovel = endImovel;
 	}
-	public String getZonaImovel() {
+	public int getZonaImovel() {
 		return zonaImovel;
 	}
-	public void setZonaImovel(String zonaImovel) {
+	public void setZonaImovel(int zonaImovel) {
 		this.zonaImovel = zonaImovel;
 	}
-	public String getTipoResidencia() {
+	public int getTipoResidencia() {
 		return tipoResidencia;
 	}
-	public void setTipoResidencia(String tipoResidencia) {
+	public void setTipoResidencia(int tipoResidencia) {
 		this.tipoResidencia = tipoResidencia;
 	}
 	
